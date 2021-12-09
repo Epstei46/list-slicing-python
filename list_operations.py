74a1864b064760b53c571afd9b53112effa11f8c
Steven Epstein <sbepstein42@gmail.com>
2021-12-08 22:37:47 -0500

    fixed most of the functions

--- list_operations.py
+++ list_operations.py
@@ -10,7 +10,7 @@ def head(input_list):
       'Jan'
     """
 
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -22,8 +22,8 @@ def tail(input_list):
     ['Feb', 'Mar']
 
     """
-
-    return []
+    new_list = input_list[1:]
+    return new_list
 
 
 def last(input_list):
@@ -36,7 +36,7 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[-1]
 
 
 def top(input_list):
@@ -49,7 +49,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 def first_three(input_list):
@@ -62,7 +62,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -75,7 +75,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +88,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +101,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +117,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -131,7 +131,7 @@ def replace_head(input_list):
     True
 
     """
-
+    input_list[0] = 42
     pass
 
 
@@ -146,7 +146,8 @@ def replace_third_and_last(input_list):
     True
 
     """
-
+    input_list[2] = 37
+    input_list[-1] = 37
     pass
 
 
