368aa4516f07dc7be5ba5ce2779faf7166ff2137
Steven Epstein <sbepstein42@gmail.com>
2021-12-09 00:24:01 -0500

    made 2 tweaks to solution

--- list_operations.py
+++ list_operations.py
@@ -146,8 +146,7 @@ def replace_third_and_last(input_list):
     True
 
     """
-    input_list[2] = 37
-    input_list[-1] = 37
+    input_list[2], input_list[-1] = 37, 37
     pass
 
 
@@ -182,7 +181,7 @@ def delete_third_and_seventh(input_list):
 
     """
     del input_list[2]
-    del input_list[5]
+    input_list[5:6] = []
     pass
 
 
